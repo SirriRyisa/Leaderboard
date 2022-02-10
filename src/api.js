2d661f42edb44fab39db54a8010c4498a81a6512
SirriRyisa <sirriryisa@gmail.com>
2022-02-10 12:30:41 +0100

    Fixed linters on HTML, CSS and JS

--- src/api.js
+++ src/api.js
@@ -1,22 +1,22 @@
-const gameApi = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/aqEWX22DXEqIpCJET4GL/scores"
+const gameApi = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/aqEWX22DXEqIpCJET4GL/scores'
 
-const getScores = async() => {
-  const response = await fetch(gameApi)
-  const data = await response.json()
+const getScores = async () => {
+  const response = await fetch(gameApi);
+  const data = await response.json();
   return data.result
-}
+};
 
-const setScores = async(obj) => {
+const setScores = async (obj) => {
   const response = await fetch(`${gameApi}`, {
     method: "POST", 
     body: JSON.stringify(obj),
     headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
+      "Content-Type": 'application/json; charset=UTF-8',
     },
   
-  }) 
-  const data = await response.json()
+  });
+  const data = await response.json();
 
-}
+};
 
-export {getScores, setScores}
+export { getScores, setScores }
